21fcd8a13b333483f0649e4314e0a4976223979a
Dis <>
2021-09-02 19:12:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
-from pygame import init, display, time, event, QUIT, Color, Surface, mouse, font, quit
+from pygame import init, display, time, event, QUIT, Color, Surface, mouse, quit
 from time import perf_counter
 from Point import Point
 from random import randint
+import pygame
 
 
-def main():
-    init()
-    surface_size = [500, 500]
-    surface = display.set_mode(surface_size, vsync=1)
-    display.set_caption('', '')
-    display.set_icon(get_icon())
-    loop(surface, time.Clock())
-    quit()
-
-
-def loop(surface, clock, points=[]):
-    text_color = Color(0, 0, 0)
-    background_color = Color(255, 255, 255)
-    my_font = font.SysFont('Segoe UI', 20)
+def loop(surface, clock, background_color=Color(255, 255, 255)):
+    points = []
+    font = get_font(background_color)
 
     while len(event.get(QUIT)) == 0:
         clock.tick(100)
@@ -25,29 +15,31 @@ def loop(surface, clock, points=[]):
         surface.fill(background_color, surface.get_rect())
         [point.update(surface, points) for point in points]
         end_frame_time = perf_counter()
-
-        info = {
-            'Objects': f'{len(points)}',
-            'Frozen': f'{percent_frozen(points)} %',
-            'Elapsed': f'{elapsed(begin_frame_time, end_frame_time)} ms.'
-        }
-
-        surface.blit(debug(info, my_font, background_color, text_color), (0, 0))
+        print_debug_info(surface, font, points, begin_frame_time, end_frame_time)
         display.flip()
         mouse_handle(
             on_left_click=lambda pos: points.append(Point(pos)),
-            on_right_click=lambda: points.clear()
+            on_right_click=points.clear
         )
 
 
-def debug(info, my_font, background_color, text_color):
+def print_debug_info(surface, font, points, begin_frame_time, end_frame_time):
+    info = {
+        'Objects': str(len(points)),
+        'Frozen': f'{percent_frozen(points)} %',
+        'Elapsed': f'{elapsed(begin_frame_time, end_frame_time)} ms.'
+    }
+    surface.blit(debug(info, font), (0, 0))
+
+
+def debug(info: dict, font: dict):
     y, shift = 0, 20
     size = 500, len(info) * (shift + 5)
     surface = Surface(size)
-    surface.fill(background_color)
+    surface.fill(font['background_color'])
     for key in info.keys():
         line = f'{key}: {info[key]}'
-        surface.blit(my_font.render(line, True, text_color), (15, y))
+        surface.blit(font['value'].render(line, True, font['color']), (15, y))
         y += shift
 
     return surface
@@ -64,10 +56,13 @@ def mouse_handle(on_left_click, on_right_click):
         on_right_click()
 
 
-def get_icon():
+def configure_display(size=(500, 500)):
+    surface = display.set_mode(size)
+    display.set_caption('')
     icon = Surface([50, 50])
     icon.fill(Color(255, 255, 255))
-    return icon
+    display.set_icon(icon)
+    return surface
 
 
 def elapsed(begin, end):
@@ -77,9 +72,23 @@ def elapsed(begin, end):
 def percent_frozen(points: list):
     if len(points) > 0:
         count_frozen = len(list(filter(lambda point: point.frozen, points)))
-        return round(count_frozen / len(points) * 100, 0)
+        return round(count_frozen / len(points) * 100, 2)
 
-    return 0.0
+    return 0.00
+
+
+def get_font(background_color):
+    return {
+        'value': pygame.font.SysFont('Segoe UI', 20),
+        'color': Color(0, 0, 0),
+        'background_color': background_color
+    }
+
+
+def main():
+    init()
+    loop(configure_display(), time.Clock())
+    quit()
 
 
 if __name__ == '__main__':
