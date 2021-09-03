e8dcdc20b2ffdb617778879583dc83601ba54291
Dis <>
2021-09-03 18:52:09 +0300

    Update Point.py

--- Point.py
+++ Point.py
@@ -5,13 +5,16 @@ from random import randint
 class Point:
     color = Color(128, 128, 128)
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, frozen=False):
         self.x, self.y = x, y
-        self.frozen = False
+        self.frozen = frozen
 
     def __contains__(self, item):
         if isinstance(item, Point):
             return (item.x, item.y) == (self.x, self.y)
+
+    def paint(self, surface):
+        draw.line(surface, Point.color, (self.x, self.y), (self.x, self.y))
 
     def update(self, surface, points):
         if not self.frozen:
@@ -19,34 +22,36 @@ class Point:
 
         self.paint(surface)
 
-    def paint(self, surface):
-        draw.line(surface, Point.color, (self.x, self.y), (self.x, self.y))
-
-    def down(self, points):
+    def down(self, points: list):
         left, down, right = self.get_targets()
-        can_down, can_left, can_right = True, True, True
+        can_down = Point.can_move(down, points)
+        can_left = Point.can_move(left, points)
+        can_right = Point.can_move(right, points)
 
-        for point in points:
-            if can_down and down in point: can_down = False
-            if can_left and left in point: can_left = False
-            if can_right and right in point: can_right = False
+        if can_down and down.y <= 499:
+            self.y = down.y
+            return
+        if can_left and left.y <= 499:
+            self.y = left.y
+            return
+        if can_right and right.y <= 499:
+            self.y = right.y
+            return
 
-        target = Point.get_target(can_down, can_left, can_right, down, left, right)
-
-        if target:
-            if target.y >= 499:
-                self.frozen = True
-
-            self.x, self.y = target.x, target.y
-
-    def get_targets(self):
-        step = randint(1, 3)
-        return Point(self.x - 1, self.y), Point(self.x, self.y + step), Point(self.x + 1, self.y)
+        if not can_down and not can_left and not can_right:
+            self.frozen = True
 
     @staticmethod
-    def get_target(can_down, can_left, can_right, down, left, right):
-        if can_down: return down
-        if can_left and can_right: return left if randint(0, 1) == 0 else right
-        if can_left: return left
-        if can_right: return right
-        return None
+    def is_busy(target, points):
+        for point in points:
+            if (point.x, point.y) == (target.x, target.y):
+                return True, point
+        return False, None
+
+    @staticmethod
+    def can_move(target, points):
+        busy, point = Point.is_busy(target, points)
+        return not busy
+
+    def get_targets(self):
+        return Point(self.x - 1, self.y + 1), Point(self.x, self.y + randint(1, 3)), Point(self.x + 1, self.y + 1)
