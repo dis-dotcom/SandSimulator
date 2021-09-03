c3b1e1fdc558d04122eaf209b6fbb96c242728e8
Dis <>
2021-09-03 22:11:32 +0300

    Update Point.py

--- Point.py
+++ Point.py
@@ -1,4 +1,5 @@
 from pygame import draw, Color
+from random import randint
 
 
 class Point:
@@ -17,9 +18,21 @@ class Point:
 
     def update(self, surface, points):
         if not self.frozen:
-            self.move_down(points)
+            if not self.move_down(points):
+                self.move_to_side(points)
 
         self.paint(surface)
+
+    def move_to_side(self, points: list):
+        left, right = Point(self.x - 1, self.y + 1), Point(self.x + 1, self.y + 1)
+        can_left = len([point for point in points if left in point]) == 0
+        can_right = len([point for point in points if right in point]) == 0
+        if can_left and can_right:
+            self.x = left.x if randint(0, 1) == 0 else right.x
+        elif can_left:
+            self.x = left.x
+        elif can_right:
+            self.x = right.x
 
     def move_down(self, points: list):
         target = Point(self.x, self.y + 1)
@@ -28,7 +41,10 @@ class Point:
 
             if self.y >= 499:
                 self.frozen = True
+
+            return True
         else:
             self.value += 1
             if self.value == 250:
                 self.frozen = True
+            return False
