fc12cc9936582e0c0c8401c8976a31be4b5feff7
Dis <>
2021-09-28 15:43:13 +0300

    Update Map.py

--- Entity/Map.py
+++ Entity/Map.py
@@ -2,13 +2,15 @@ class Map:
     def __init__(self, points, size):
         self.__points__ = points
         self.__map__ = []
+        self.__columns__ = 0
+        self.__rows__ = 0
         self.set_size(size, size)
 
+    def has_index(self, x, y):
+        return y < self.__rows__ and x < self.__columns__
+
     def __contains__(self, item):
-        try:
-            return self.__map__[item.x][item.y] is not None
-        except:
-            return False
+        return self.has_index(item.x, item.y) and self.__map__[item.x][item.y] is not None
 
     def __getitem__(self, item):
         return self.__map__[item]
@@ -21,6 +23,8 @@ class Map:
         self.__map__.clear()
 
     def set_size(self, rows, columns):
+        self.__rows__ = rows
+        self.__columns__ = columns
         [self.__map__.append([None for _ in range(columns)]) for _ in range(rows)]
 
     def apply_for_point(self, x, y, condition, action):
