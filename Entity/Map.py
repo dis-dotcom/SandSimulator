7f00b4abb3a5febfd56d3b018611a49ac090695a
Dis <>
2021-09-28 14:54:25 +0300

    Update Map.py

--- Entity/Map.py
+++ Entity/Map.py
@@ -1,14 +1,27 @@
 class Map:
-    def __init__(self):
-        self.map = []
+    def __init__(self, points, size):
+        self.__points__ = points
+        self.__map__ = []
+        self.set_size(size, size)
+
+    def __contains__(self, item):
+        x, y = item.x, item.y
+        if self.__map__
 
     def __getitem__(self, item):
-        return self.map[item]
+        return self.__map__[item]
+
+    def points(self):
+        return self.__points__
+
+    def clear(self):
+        self.__points__.clear()
+        self.__map__.clear()
 
     def set_size(self, rows, columns):
-        [self.map.append([None for _ in range(columns)]) for _ in range(rows)]
+        [self.__map__.append([None for _ in range(columns)]) for _ in range(rows)]
 
-    def set_points(self, points):
-        for point in points:
-            x, y = point.x, point.y
-            self[x][y] = point
+    def apply_for_point(self, x, y, condition, action):
+        point = self.__map__[x][y]
+        if condition(point):
+            action()
